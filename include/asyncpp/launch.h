7c88b394ca17614d22167113395834ebbb8620df
Dominik Thalhammer <dominik@thalhammer.it>
2023-05-16 12:30:43 +0200

    Extend async_launch_scope

--- include/asyncpp/launch.h
+++ include/asyncpp/launch.h
@@ -3,7 +3,10 @@
 #include <asyncpp/scope_guard.h>
 #include <atomic>
 #include <cassert>
+#include <exception>
+#include <future>
 #include <stdexcept>
+#include <tuple>
 #include <utility>
 
 namespace asyncpp {
@@ -79,6 +82,45 @@ namespace asyncpp {
 		}
 
 		/**
+		 * \brief Invoke the provided callable in a new task. The callable is
+		 *		  copied into the task, making it save for usage with e.g. lambda captures.
+		 * \param callable The callable to invoke
+		 * \param args The arguments to pass into callable on invocation
+		 * \param allocator Allocator used for allocating the wrapper task
+		 */
+		template<typename Callable, typename... Args, ByteAllocator Allocator = default_allocator_type>
+			requires(std::is_invocable_v<Callable, Args...>)
+		void invoke_tuple(Callable&& callable, std::tuple<Args...>&& args, const Allocator& allocator = {}) {
+			[](async_launch_scope* scope, Callable callable, std::tuple<Args...>&& args,
+			   const Allocator& allocator) -> detail::launch_task<Allocator> {
+				scope->m_count.fetch_add(1);
+				scope_guard guard{[scope]() noexcept {
+					// If this is the last task
+					if (scope->m_count.fetch_sub(1) == 1) {
+						// And we are being awaited
+						auto hdl = scope->m_continuation.exchange(nullptr);
+						// Resume the awaiter
+						if (hdl != nullptr) coroutine_handle<>::from_address(hdl).resume();
+					}
+				}};
+				co_await std::apply(std::move(callable), std::move(args));
+			}(this, std::forward<Callable>(callable), std::move(args), allocator);
+		}
+
+		/**
+		 * \brief Invoke the provided callable in a new task. The callable is
+		 *		  copied into the task, making it save for usage with e.g. lambda captures.
+		 * \note If you need to use a custom allocator use the tuple overload
+		 * \param callable The callable to invoke
+		 * \param args The arguments to pass into callable on invocation
+		 */
+		template<typename Callable, typename... Args>
+			requires(std::is_invocable_v<Callable, Args...>)
+		void invoke(Callable&& callable, Args&&... args) {
+			invoke_tuple(std::forward<Callable>(callable), std::forward_as_tuple<Args&&...>(args...));
+		}
+
+		/**
 		 * \brief Wait for all active tasks to finish
 		 * \return auto Awaiter that pauses the current coroutine until all spawned task have finished.
 		 */
@@ -100,6 +142,24 @@ namespace asyncpp {
 				constexpr void await_resume() const noexcept {}
 			};
 			return awaiter{this};
+		}
+
+		/**
+		 * \brief Create a future that finishes once all tasks are done.
+		 *			This is equivalent to awaiting join() in a promise.
+		 * \return std::future waiting for all tasks
+		 */
+		template<ByteAllocator Allocator = default_allocator_type>
+		std::future<void> join_future(const Allocator& allocator = {}) noexcept {
+			std::promise<void> done;
+			auto res = done.get_future();
+			[](async_launch_scope* that, std::promise<void> done, const Allocator&) -> detail::launch_task<Allocator> {
+				try {
+					co_await that->join();
+					done.set_value();
+				} catch (...) { done.set_exception(std::current_exception()); }
+			}(this, std::move(done), allocator);
+			return res;
 		}
 
 		/**
