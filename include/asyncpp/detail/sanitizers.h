555007f5c16c6de9be281b2eaa5b196d46f310d4
Dominik Thalhammer <dominik@thalhammer.it>
2024-04-18 19:29:15 +0200

    Sanitizer detection

--- include/asyncpp/detail/sanitizers.h
+++ include/asyncpp/detail/sanitizers.h
@@ -1,22 +1,32 @@
 #pragma once
 
 #ifndef ASYNCPP_HAS_ASAN
-#if (defined(__has_feature) && __has_feature(address_sanitizer)) || defined(__SANITIZE_ADDRESS__)
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer)
 #define ASYNCPP_HAS_ASAN 1
-#else
-#define ASYNCPP_HAS_ASAN 0
 #endif
+#elif defined(__SANITIZE_ADDRESS__)
+#define ASYNCPP_HAS_ASAN 1
+#endif
+#endif
+#ifndef ASYNCPP_HAS_ASAN
+#define ASYNCPP_HAS_ASAN 0
 #endif
 #if ASYNCPP_HAS_ASAN
 #include <sanitizer/asan_interface.h>
 #endif
 
 #ifndef ASYNCPP_HAS_TSAN
-#if (defined(__has_feature) && __has_feature(thread_sanitizer)) || defined(__SANITIZE_THREAD__)
+#if defined(__has_feature)
+#if __has_feature(thread_sanitizer)
 #define ASYNCPP_HAS_TSAN 1
-#else
-#define ASYNCPP_HAS_TSAN 0
 #endif
+#elif defined(__SANITIZE_THREAD__)
+#define ASYNCPP_HAS_TSAN 1
+#endif
+#endif
+#ifndef ASYNCPP_HAS_TSAN
+#define ASYNCPP_HAS_TSAN 0
 #endif
 #if ASYNCPP_HAS_TSAN
 #include <sanitizer/tsan_interface.h>
