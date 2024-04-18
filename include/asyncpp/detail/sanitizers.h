79176f8de6ea9e08b45884477e068baf12101db0
Dominik Thalhammer <dominik@thalhammer.it>
2024-04-18 19:22:48 +0200

    :bug: Fix sanitizer detection on gcc

--- include/asyncpp/detail/sanitizers.h
+++ include/asyncpp/detail/sanitizers.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef ASYNCPP_HAS_ASAN
-#if __has_feature(address_sanitizer)
+#if (defined(__has_feature) && __has_feature(address_sanitizer)) || defined(__SANITIZE_ADDRESS__)
 #define ASYNCPP_HAS_ASAN 1
 #else
 #define ASYNCPP_HAS_ASAN 0
@@ -12,7 +12,7 @@
 #endif
 
 #ifndef ASYNCPP_HAS_TSAN
-#if __has_feature(thread_sanitizer)
+#if (defined(__has_feature) && __has_feature(thread_sanitizer)) || defined(__SANITIZE_THREAD__)
 #define ASYNCPP_HAS_TSAN 1
 #else
 #define ASYNCPP_HAS_TSAN 0
