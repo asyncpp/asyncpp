95d99fac39839f58d44546facc616341444f4415
Dominik Thalhammer <dominik@thalhammer.it>
2024-06-14 11:55:17 +0200

    :bug: Various minor improvements

--- test/launch.cpp
+++ test/launch.cpp
@@ -54,3 +54,45 @@ TEST(ASYNCPP, AsyncLaunchScope) {
 	ASSERT_TRUE(join_did_return);
 	ASSERT_FALSE(d.invoke_all());
 }
+
+TEST(ASYNCPP, AsyncLaunchScopeInvoke) {
+	struct test_dispatcher {
+		std::vector<std::function<void()>> waiting;
+		void push(std::function<void()> fn) { waiting.emplace_back(std::move(fn)); }
+		bool invoke_all() {
+			auto w = std::move(waiting);
+			for (auto& e : w)
+				e();
+			return !w.empty();
+		}
+	};
+	struct destroy_tracker {
+		bool* ptr;
+		constexpr explicit destroy_tracker(bool* pval) noexcept : ptr(pval) {}
+		destroy_tracker(const destroy_tracker&) = delete;
+		destroy_tracker& operator=(const destroy_tracker&) = delete;
+		destroy_tracker(destroy_tracker&& other) noexcept : ptr(other.ptr) {
+			other.ptr = nullptr;
+		}
+		destroy_tracker& operator=(destroy_tracker&& other) noexcept  {
+			ptr = other.ptr;
+			other.ptr = nullptr;
+			return *this;
+		}
+		~destroy_tracker() {
+			if(ptr != nullptr) *ptr = true;
+		}
+	};
+	test_dispatcher d{};
+	bool did_destroy = false;
+	async_launch_scope scope{};
+
+	scope.invoke([t = destroy_tracker{&did_destroy}](test_dispatcher& d) -> task<void> {
+		co_await defer{d};
+	}, d);
+
+	ASSERT_FALSE(did_destroy);
+	ASSERT_TRUE(d.invoke_all());
+
+	ASSERT_TRUE(did_destroy);
+}
---
+++ test/so_compat.cpp
@@ -0,0 +1,5 @@
+#ifdef ASYNCPP_SO_COMPAT
+#define ASYNCPP_SO_COMPAT_IMPL
+#endif
+#include <asyncpp/dispatcher.h>
+#include <asyncpp/fiber.h>
